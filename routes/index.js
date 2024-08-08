29919b5ee2f6a6fe5802c452282e346154ce073f
j2136 <167533952+j2136@users.noreply.github.com>
2024-08-08 08:07:50 +0000

    課題終わり

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,15 @@ const knex = require('../db/knex');
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
   const isAuth = Boolean(userId);
-  knex("tasks")
-    .select("*")
+  const search = req.query.search || '';
+
+  knex('tasks')
+    .select('*')
     .then(function (results) {
+      const filteredResults = results.filter(task => task.content.includes(search));
       res.render('index', {
         title: 'ToDo App',
-        todos: results,
+        todos: filteredResults,
         isAuth: isAuth,
       });
     })
@@ -27,6 +30,9 @@ router.post('/', function (req, res, next) {
   const userId = req.session.userid;
   const isAuth = Boolean(userId);
   const todo = req.body.add;
+  if (!todo) {
+    return res.redirect('/');
+  }
   knex("tasks")
     .insert({user_id: 1, content: todo})
     .then(function () {
@@ -61,6 +67,20 @@ router.post('/complete/:id', function (req, res, next) {
     .where('id', id)
     .update({ completed: true })
     .then(function () {
+      res.redirect('/');
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.redirect('/');
+    });
+});
+
+router.post('/edit', function (req, res, next) {
+  const { id, content } = req.body;
+  knex('tasks')
+    .where('id', id)
+    .update({ content: content })
+    .then(() => {
       res.redirect('/');
     })
     .catch(function (err) {
